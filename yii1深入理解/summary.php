7376cc610dda8aeddf339fa9e27d13ecd9545289
frd <frd@localhost>
2016-11-22 18:08:49 +0800

    update

---
+++ yii1深入理解/summary.php
@@ -0,0 +1,4 @@
+<?php
+   return array(
+      array('自动载入机制','autoload.md'),
+   );
---
+++ 杂项/summary.php
@@ -0,0 +1,12 @@
+<?php
+   return array(
+      array('Introduction','README.md'),
+      array('helloworld','helloworld.md'),
+      array('major classes','major_classes.md'),
+      array('qt designer','qt_designer.md'),
+      array('signals','signals.md'),
+      array('layouts','layouts.md'),
+      array('widgets','widgets.md'),
+      array('dialog','dialog.md'),
+      array('messageBox','messageBox.md'),
+   );
