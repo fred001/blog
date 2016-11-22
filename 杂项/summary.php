e890e799ff16d52ed2c498a8229096e1ec67b81b
frd <frd@localhost>
2016-11-23 07:22:44 +0800

    update

--- 杂项/summary.php
+++ 杂项/summary.php
@@ -1,12 +1,8 @@
 <?php
    return array(
       array('Introduction','README.md'),
-      array('helloworld','helloworld.md'),
-      array('major classes','major_classes.md'),
-      array('qt designer','qt_designer.md'),
-      array('signals','signals.md'),
-      array('layouts','layouts.md'),
-      array('widgets','widgets.md'),
-      array('dialog','dialog.md'),
-      array('messageBox','messageBox.md'),
+      array('杂项',array(
+         array('gitbook 安装','gitbook_install.md'),
+         array('nginx+awstats 安装配置','nginx+awstats.md'),
+      )),
    );
