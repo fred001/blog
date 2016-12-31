eccbfa03597c693f2e99ac9abd28ef6092979d6b
frd <frd@localhost>
2016-12-31 16:45:46 +0800

    update

--- yii1深入理解/summary.php
+++ yii1深入理解/summary.php
@@ -1,4 +1,13 @@
 <?php
    return array(
       array('自动载入机制','autoload.md'),
+      array('actions.md', 'actions.md'),
+      array('all.md', 'all.md'),
+      array('CFormModel.md', 'CFormModel.md'),
+      array('db.md', 'db.md'),
+      array('gii.md', 'gii.md'),
+      array('tmp.md', 'tmp.md'),
+      array('运行流程.md', '运行流程.md'),
    );
+
+
